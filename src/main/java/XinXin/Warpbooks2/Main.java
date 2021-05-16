0f655c31f64fba77e72a3af05b5fceffcef5c0e4
sebdomsan54 <sebdomsan54@yahoo.com>
2021-05-16 18:00:47 -0400

    Updated

--- src/main/java/XinXin/Warpbooks2/Main.java
+++ src/main/java/XinXin/Warpbooks2/Main.java
@@ -1,32 +1,16 @@
 package XinXin.Warpbooks2;
 
-import net.minecraft.server.v1_16_R3.ContainerPlayer;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener{
+public class Main extends JavaPlugin{
     @Override
     public void onEnable() {
-        getLogger().info("Yeet on");
-        getServer().getPluginManager().registerEvents((Listener) this, this);
+        getLogger().info("Warpbook V2 has loaded");
     }
     @Override
     public void onDisable() {
-        getLogger().info("Yeet off");
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-
-        Bukkit.broadcastMessage("your message");
-
+        getLogger().info("Warpbook V2 has unloaded");
     }
 
 }

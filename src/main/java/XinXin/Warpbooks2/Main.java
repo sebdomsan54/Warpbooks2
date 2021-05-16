48ac524107e19de570c628a241e9758210068877
sebdomsan54 <sebdomsan54@yahoo.com>
2021-05-16 18:41:42 -0400

    Updated

--- src/main/java/XinXin/Warpbooks2/Main.java
+++ src/main/java/XinXin/Warpbooks2/Main.java
@@ -1,11 +1,20 @@
 package XinXin.Warpbooks2;
 
+import main.java.XinXin.Warpbooks2.EventsListeners;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
+
+public class Main extends JavaPlugin {
+    //Sets up the var plugin to refrance this class
+    static Main plugin;
+    public Main() {
+        plugin = this;
+    }
+
     @Override
     public void onEnable() {
+        plugin.registerEvents();
         getLogger().info("Warpbook V2 has loaded");
     }
     @Override
@@ -13,4 +22,7 @@ public class Main extends JavaPlugin{
         getLogger().info("Warpbook V2 has unloaded");
     }
 
+    private void registerEvents() {
+        Bukkit.getPluginManager().registerEvents(new EventsListeners(), this);
+    }
 }

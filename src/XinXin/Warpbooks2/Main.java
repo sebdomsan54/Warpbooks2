8d5daaf61349ab273e44dbfee55c28ad7326e0b8
sebdomsan54 <sebdomsan54@yahoo.com>
2021-05-16 14:51:29 -0400

    Initial commit

---
+++ src/XinXin/Warpbooks2/Main.java
@@ -0,0 +1,15 @@
+package XinXin.Warpbooks2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Yeet on");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Yeet off");
+    }
+}

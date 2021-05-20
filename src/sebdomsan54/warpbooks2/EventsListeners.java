235891488fbd1504844e62df7349331a3214e593
sebdomsan54 <sebdomsan54@yahoo.com>
2021-05-19 21:58:46 -0400

    update

--- src/sebdomsan54/warpbooks2/EventsListeners.java
+++ src/sebdomsan54/warpbooks2/EventsListeners.java
@@ -1,24 +1,23 @@
 package sebdomsan54.warpbooks2;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.Objects;
 
 public class EventsListeners implements Listener{
 
 
 
     @EventHandler
-    public void onPlayerClicks(PlayerInteractEvent event) throws InterruptedException {
+    public void onPlayerClicks(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         Action action = event.getAction();
-        if(action.toString() == "RIGHT_CLICK_AIR" || action.toString() == "RIGHT_CLICK_BLOCK"){
-            player.sendMessage(event.getItem().toString());
+        if(action.toString().equals("RIGHT_CLICK_AIR") || action.toString().equals("RIGHT_CLICK_BLOCK")){
+            player.sendMessage(Objects.requireNonNull(event.getItem()).toString());
         }
         event.setCancelled(true);
         event.setCancelled(false);
